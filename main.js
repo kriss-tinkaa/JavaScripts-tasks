272f91cfedf421d2b2fa6074e1b20e71db4b8424
Kristina <44407472+kriss-tinkaa@users.noreply.github.com>
2021-11-23 20:50:02 +0200

    toInitials

--- main.js
+++ main.js
@@ -8,20 +8,33 @@ function isPalindrom(str) {
     return res;
 }
 
-console.log(isPalindrom('тест'));
-console.log(isPalindrom('шалаш'));
+// console.log(isPalindrom('тест')); //  false
+// console.log(isPalindrom('шалаш')); // true
 
 
 //Ф-ция поиска короткого слова
 
 function findShort(str){
     let wordsArr = str.split(' ');
-    console.log(wordsArr)
     let sortedWordssArr = wordsArr.sort(function(a,b){
         return a.length - b.length;
     });
     return sortedWordssArr[0];
 }
 
-console.log(findShort('THe Smallest word in sentence'));// in
-console.log(findShort('Just test string'))// just
+// console.log(findShort('THe Smallest word in sentence'));// in
+// console.log(findShort('Just test string'))// just
+
+// Ф-ция создания инициалов 
+
+function toInitials(name){
+    let nameArr = name.split(' ');
+    let firstLetterArr = nameArr.map(function(el){
+        return el.slice(0, 1).toUpperCase() + '.';
+    });
+    let initials = firstLetterArr.join('');
+    return initials;
+}
+
+console.log(toInitials('Bill Gates'))// B.G.
+console.log(toInitials('elon mask'))// E.M.
