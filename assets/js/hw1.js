1d644c37f8c165da24e4abd821ec649be1e1bc7d
kradchenkonlt <44407472+kriss-tinkaa@users.noreply.github.com>
2022-05-23 08:13:40 +0200

    Update hw1.js

--- assets/js/hw1.js
+++ assets/js/hw1.js
@@ -2,7 +2,7 @@
 
 let arr = ['a', 'b', 'c'];
 
-/**  Task1 **/
+/**  Task 1 **/
 // ДанІ два массива: ['a', 'b', 'c'] и [1, 2, 3]. Об’єднати їх разом.
 
 
@@ -12,7 +12,7 @@ const children = arr.concat(arr2);
 //console.log(children)
 
 
-/**  Task2 **/
+/**  Task 2 **/
 // Дано массив ['a', 'b', 'c']. Добавте йому в кінець елеменети 1, 2, 3.
 
 
@@ -21,25 +21,28 @@ const arrEnd = arr.push( 1, 2, 3);
 
 
 
-/**  Task3 **/
+/**  Task 3 **/
 //Дан массив [3, 4, 1, 2, 7]. Відсортуйте його.
 
 let arrSort = [3, 4, 1, 2, 7];
 arrSort.sort((number1, number2) => number1 - number2);
 //console.log(arrSort)
 
-/**  Task4 **/
+/**  Task 4 **/
 //Reverse function
 
-let reverse = (arr) => {
+function reverse(arr){
   let newArr = [];
-  for(let i = 0; i < arr.lengh; i++){
-    //console.log(arr[i])
-    console.log(arr[i])
+  for( let i = 0; i < arr.length; i++){
+      newArr.unshift(arr[i]);
   }
-  return newArr
-}
-
+  return newArr;
+} 
 console.log(reverse( [1,2,3,4]))
 
+
+/**  Task 5 **/
+//Чему равен typeof null в режиме use strict?
+console.log(typeof null)
+// object
 
