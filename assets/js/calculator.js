b9e39aa3c56000d24b0bb9f453a54266eff5376f
kradchenkonlt <44407472+kriss-tinkaa@users.noreply.github.com>
2022-06-05 12:04:04 +0200

    Add calculator

---
+++ assets/js/calculator.js
@@ -0,0 +1,53 @@
+let a = '';
+let b = '';
+let sign = '';
+let finish = false;
+
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['-', '+', 'X', '/'];
+
+const out = document.querySelector('.calc-screen p');
+
+function clearAll() {
+    a = '';
+    b = '';
+    sign = '';
+    finish = false;
+    out.textContent = 0;
+    console.log('clear')
+}
+
+document.querySelector('.ac').onclick = clearAll;
+
+document.querySelector('.buttons').onclick = (event) => {
+    // key doen't press
+    if(!event.target.classList.contains('btn')) return;
+    //  ac key was pressed
+    if(event.target.classList.contains('ac')) return; 
+
+    out.textContent = '';
+    // take press button 
+    const key = event.target.textContent;
+
+    //if press buttons 0-9 or .
+    if(digit.includes(key)) {
+       if(b === '' && sign === ''){
+        a += key;
+        out.textContent = a;
+       } else if (a !== '' && b !== '' && finish ) {
+
+       } else {
+           b += key;
+       }
+       console.log(a, b, sign);
+       return;
+    }
+
+    //if press buttons + - / *
+    if(action.includes(key)){
+        sign = key;
+        out.textContent = sign;
+        console.log(a, b, sign)
+        return;
+    }
+}
